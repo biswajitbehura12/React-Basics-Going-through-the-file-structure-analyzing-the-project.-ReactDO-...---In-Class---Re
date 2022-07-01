8cde76e2394d1d91a90fe8eaeff49d55f70eb7de
BISWAJIT BEHURA <102812316+biswajitbehura12@users.noreply.github.com>
2022-07-01 14:30:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main">
+    <div id="data-ns-test">
     <p data-ns-test="project-name">jhkdshshk</p>
     <p data-ns-test="project-description">dxvkjdxvljs</p>
     </div>
